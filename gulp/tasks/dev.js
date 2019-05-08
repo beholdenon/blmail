1d0233de945f933e3419183e8c6d5de942def92a
Brian Holden <brian.holden@bloomingdales.com>
2019-05-08 13:50:57 -0400

    Initial commit

---
+++ gulp/tasks/dev.js
@@ -0,0 +1,16 @@
+var connect = require( 'gulp-connect' );
+
+// Export functions
+exports.server = server;
+
+// Start server w/ live reload
+function server( done ) {
+	connect.server( {
+		port: 8081,
+		root: 'dist/',
+		livereload: true,
+		host: '11.177.174.75',
+	} );
+
+	done();
+}
